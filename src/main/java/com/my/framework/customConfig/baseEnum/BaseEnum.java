4278212612cacfcb10ac7612af7d01002c713131
wkn <wkn4122779>
2020-12-21 16:54:35 +0800

    添加BaseEnum

---
+++ src/main/java/com/my/framework/customConfig/baseEnum/BaseEnum.java
@@ -0,0 +1,8 @@
+package com.my.framework.customConfig.baseEnum;
+
+public interface BaseEnum {
+
+    String getCode();
+
+    String getMessage();
+}
---
+++ src/main/java/com/my/framework/customConfig/baseEnum/EnumFormatters.java
@@ -0,0 +1,14 @@
+package com.my.framework.customConfig.baseEnum;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class EnumFormatters implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(new StringToEnumConverterFactory());
+    }
+}
