58bc6bed960e9168dfe5453d0303849ccb42bc87
wkn <wkn4122779>
2021-01-27 17:10:22 +0800

    新增请求第三方接口get post方法

---
+++ src/main/java/com/my/framework/service/RequestService.java
@@ -0,0 +1,46 @@
+package com.my.framework.service;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RequestService {
+
+    private final RestTemplate restTemplate;
+
+    public RequestService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public JSONObject get(String url, LinkedMultiValueMap<String, String> params, String token) {
+        URI uri = UriComponentsBuilder.fromUriString(url).queryParams(params).build().toUri();
+        HttpEntity<Void> httpEntity = token == null ? null : httpEntity(token);
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, JSONObject.class).getBody();
+    }
+
+    public JSONObject post(String url, LinkedMultiValueMap<String, String> params, String token) {
+        URI uri = UriComponentsBuilder.fromUriString(url).queryParams(params).build().toUri();
+        HttpEntity<Void> httpEntity = token == null ? null : httpEntity(token);
+        return restTemplate.exchange(uri, HttpMethod.POST, httpEntity, JSONObject.class).getBody();
+    }
+
+    public JSONObject request(String url, HttpMethod httpMethod, LinkedMultiValueMap<String, String> params, String token) {
+        URI uri = UriComponentsBuilder.fromUriString(url).queryParams(params).build().toUri();
+        HttpEntity<Void> httpEntity = token == null ? null : httpEntity(token);
+        return restTemplate.exchange(uri, httpMethod, httpEntity, JSONObject.class).getBody();
+    }
+
+    private HttpEntity<Void> httpEntity(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", token);
+        return new HttpEntity<>(headers);
+    }
+}
